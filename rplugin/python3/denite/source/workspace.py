d4aa2acde8074c7a7e1c683f623ba273c302291d
ricky_saurav <luckyricky007@gmail.com>
2020-04-16 04:43:54 +0530

    Fix workspace symbols denite source

--- rplugin/python3/denite/source/workspace.py
+++ rplugin/python3/denite/source/workspace.py
@@ -9,44 +9,87 @@ from denite.kind.file import Kind as FileKind
 from denite.source.base import Base
 from os.path import relpath
 
-class Source(Base):
+symbolKinds = [
+    "File",
+    "Module",
+    "Namespace",
+    "Package",
+    "Class",
+    "Method",
+    "Property",
+    "Field",
+    "Constructor",
+    "Enum",
+    "Interface",
+    "Function",
+    "Variable",
+    "Constant",
+    "String",
+    "Number",
+    "Boolean",
+    "Array",
+    "Object",
+    "Key",
+    "Null",
+    "EnumMember",
+    "Struct",
+    "Event",
+    "Operator",
+    "TypeParameter",
+]
+symbol_kind_map = {i + 1: v for i, v in enumerate(symbolKinds)}
 
+
+class Source(Base):
     def __init__(self, vim):
         super().__init__(vim)
-
-        self.name = 'coc-workspace'
+        self.name = "coc-workspace"
         self.kind = FileKind(vim)
 
     def define_syntax(self):
-        self.vim.command('syntax case ignore')
-        self.vim.command(r'syntax match deniteSource_WorkspaceHeader /\v^.*$/ containedin=' + self.syntax_name)
-        self.vim.command(r'syntax match deniteSource_WorkspaceName /\v^\s*\S+/ contained '
-                         r'containedin=deniteSource_WorkspaceHeader')
-        self.vim.command(r'syntax match deniteSource_WorkspaceKind /\[\w\+\]/ contained '
-                         r'containedin=deniteSource_WorkspaceHeader')
-        self.vim.command(r'syntax match deniteSource_WorkspaceFile /\f\+$/ contained '
-                         r'containedin=deniteSource_WorkspaceHeader')
-
+        self.vim.command("syntax case ignore")
+        self.vim.command(
+            r"syntax match deniteSource_WorkspaceHeader /\v^.*$/ containedin="
+            + self.syntax_name
+        )
+        self.vim.command(
+            r"syntax match deniteSource_WorkspaceName /\v^\s*\S+/ contained "
+            r"containedin=deniteSource_WorkspaceHeader"
+        )
+        self.vim.command(
+            r"syntax match deniteSource_WorkspaceKind /\[\w\+\]/ contained "
+            r"containedin=deniteSource_WorkspaceHeader"
+        )
+        self.vim.command(
+            r"syntax match deniteSource_WorkspaceFile /\f\+$/ contained "
+            r"containedin=deniteSource_WorkspaceHeader"
+        )
 
     def highlight(self):
-        self.vim.command('highlight default link deniteSource_WorkspaceName Normal')
-        self.vim.command('highlight default link deniteSource_WorkspaceKind Typedef')
-        self.vim.command('highlight default link deniteSource_WorkspaceFile Comment')
+        self.vim.command("highlight default link deniteSource_WorkspaceName Normal")
+        self.vim.command("highlight default link deniteSource_WorkspaceKind Typedef")
+        self.vim.command("highlight default link deniteSource_WorkspaceFile Comment")
+    def on_init(self,context):
+        context['is_interactive'] = True
 
     def gather_candidates(self, context):
-        cwd = self.vim.call('getcwd')
-        items = self.vim.call('CocAction', 'workspaceSymbols')
-        if items is None or items is 0:
+        items = self.vim.call("CocAction", "getWorkspaceSymbols", context["input"],context['bufnr'])
+        if not items:
             return []
         candidates = []
         for item in items:
-            filepath = relpath(item['filepath'], start=cwd)
-            candidates.append({
-                'word': item['text'],
-                'abbr': '%s [%s] %s' % (item['text'], item['kind'], filepath),
-                'action__path': item['filepath'],
-                'action__col': item['col'],
-                'action__line': item['lnum'],
-                })
-
+            name = item["name"]
+            kind = symbol_kind_map.get(item["kind"],"Unknown")
+            file_path = item["location"]["uri"]
+            lnum = item["location"]["range"]["start"]["line"]+1
+            col = item["location"]["range"]["start"]["character"]
+            candidates.append(
+                {
+                    "word": name,
+                    "abbr": "%s [%s] %s" % (name, kind, file_path.replace("file://","")),
+                    "action__path": file_path,
+                    "action__col": col,
+                    "action__line": lnum,
+                }
+            )
         return candidates
